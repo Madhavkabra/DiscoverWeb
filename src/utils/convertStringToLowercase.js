219719a6efacf8ea22ac71cb824ed85c7b4ea10d
Madhav K <madhav.kabra1100@gmail.com>
2022-12-12 20:51:28 +0530

    Add util to convert string in lower case

---
+++ src/utils/convertStringToLowercase.js
@@ -0,0 +1,6 @@
+/**
+ *
+ * @param {string} str A string to convert in lowercase.
+ * @returns string with lower case letters.
+ */
+export const convertStringToLowercase = (str) => str.toLowerCase();
