6fb61973daf047f8d99b0b59506a2a9e2df3835e
Madhav K <madhav.kabra1100@gmail.com>
2022-12-12 15:20:57 +0530

    Installed required dependencies and implemented Mui table

---
+++ src/Components/CustomTable/CustomTable.jsx
@@ -0,0 +1,135 @@
+import React from "react";
+import Box from "@mui/material/Box";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import {
+  createColumnHelper,
+  flexRender,
+  useReactTable,
+  getCoreRowModel,
+} from "@tanstack/react-table";
+
+const defaultData = [
+  {
+    id: "1",
+    firstName: "tanner",
+    lastName: "linsley",
+    address: "India",
+    state: "India",
+    phoneNumber: "9999999999",
+  },
+  {
+    id: "2",
+    firstName: "tandy",
+    lastName: "miller",
+    address: "India",
+    state: "India",
+    phoneNumber: "9999999999",
+  },
+  {
+    id: "3",
+    firstName: "joe",
+    lastName: "dirte",
+    address: "India",
+    state: "India",
+    phoneNumber: "9999999999",
+  },
+];
+
+const columnHelper = createColumnHelper();
+const columns = [
+  columnHelper.accessor("id", {
+    header: () => "#",
+    id: "id",
+    cell: (info) => info.getValue(),
+    footer: (info) => info.column.id,
+  }),
+  columnHelper.accessor("firstName", {
+    id: "firstName",
+    header: () => "First Name",
+    cell: (info) => info.getValue(),
+    footer: (info) => info.column.id,
+  }),
+  columnHelper.accessor((row) => row.lastName, {
+    id: "lastName",
+    header: () => "Last Name",
+    cell: (info) => <i>{info.getValue()}</i>,
+    footer: (info) => info.column.id,
+  }),
+  columnHelper.accessor("address", {
+    id: "address",
+    header: () => "Address",
+    cell: (info) => info.renderValue(),
+    footer: (info) => info.column.id,
+  }),
+  columnHelper.accessor("state", {
+    id: "state",
+    header: () => "State",
+    footer: (info) => info.column.id,
+  }),
+  columnHelper.accessor("phoneNumber", {
+    id: "phoneNumber",
+    header: "Phone Number",
+    footer: (info) => info.column.id,
+  }),
+];
+
+const CustomTable = () => {
+  const [data, setData] = React.useState(() => [...defaultData]);
+
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
+  return (
+    <Box sx={{ width: "100%" }}>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableCell key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                    </TableCell>
+                  );
+                })}
+              </TableRow>
+            ))}
+          </TableHead>
+          <TableBody>
+            {table.getRowModel().rows.map((row) => {
+              return (
+                <TableRow key={row.id}>
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Box>
+  );
+};
+
+export default CustomTable;
---
+++ src/Page/Table.page.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import CustomTable from "../Components/CustomTable/CustomTable";
+
+const TablePage = () => {
+  return <CustomTable />;
+};
+
+export default TablePage;
