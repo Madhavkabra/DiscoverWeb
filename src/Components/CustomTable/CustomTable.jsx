386d3fce76969f0fa4fddc4ede6854a04b430ff7
Madhav K <madhav.kabra1100@gmail.com>
2022-12-12 20:38:29 +0530

    API calling and text field design

--- src/Components/CustomTable/CustomTable.jsx
+++ src/Components/CustomTable/CustomTable.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import Box from "@mui/material/Box";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
@@ -8,78 +8,58 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import Button from "@mui/material/Button";
+import Input from "@mui/material/Input";
 import IconButton from "@mui/material/IconButton";
 import PushPinIcon from "@mui/icons-material/PushPin";
 import CancelIcon from "@mui/icons-material/Cancel";
+import InputAdornment from "@mui/material/InputAdornment";
+import SearchIcon from "@mui/icons-material/Search";
+import CloseIcon from "@mui/icons-material/Close";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
 import { useTheme } from "@mui/material/styles";
 import {
   createColumnHelper,
   flexRender,
   useReactTable,
   getCoreRowModel,
+  getSortedRowModel,
 } from "@tanstack/react-table";
 import { ColorModeContext } from "../../App";
-
-const defaultData = [
-  {
-    id: "1",
-    firstName: "tanner",
-    lastName: "linsley",
-    address: "India",
-    state: "India",
-    phoneNumber: "9999999999",
-  },
-  {
-    id: "2",
-    firstName: "tandy",
-    lastName: "miller",
-    address: "India",
-    state: "India",
-    phoneNumber: "9999999999",
-  },
-  {
-    id: "3",
-    firstName: "joe",
-    lastName: "dirte",
-    address: "India",
-    state: "India",
-    phoneNumber: "9999999999",
-  },
-];
+import { getUsers } from "../../services/axios/users";
 
 const columnHelper = createColumnHelper();
 const columns = [
   columnHelper.accessor("id", {
-    header: () => "#",
+    header: "#",
     id: "id",
     cell: (info) => info.getValue(),
     footer: (info) => info.column.id,
   }),
   columnHelper.accessor("firstName", {
     id: "firstName",
-    header: () => "First Name",
+    header: "First Name",
     cell: (info) => info.getValue(),
     footer: (info) => info.column.id,
   }),
   columnHelper.accessor((row) => row.lastName, {
     id: "lastName",
-    header: () => "Last Name",
+    header: "Last Name",
     cell: (info) => <i>{info.getValue()}</i>,
     footer: (info) => info.column.id,
   }),
   columnHelper.accessor("address", {
     id: "address",
-    header: () => "Address",
+    header: "Address",
     cell: (info) => info.renderValue(),
     footer: (info) => info.column.id,
   }),
   columnHelper.accessor("state", {
     id: "state",
-    header: () => "State",
+    header: "State",
     footer: (info) => info.column.id,
   }),
-  columnHelper.accessor("phoneNumber", {
-    id: "phoneNumber",
+  columnHelper.accessor("phone", {
+    id: "phone",
     header: "Phone Number",
     footer: (info) => info.column.id,
   }),
@@ -88,21 +68,35 @@ const columns = [
 const CustomTable = () => {
   const theme = useTheme();
   const colorMode = React.useContext(ColorModeContext);
-  const [data, setData] = React.useState(() => [...defaultData]);
+  const [data, setData] = React.useState([]);
   const [columnPinning, setColumnPinning] = React.useState({});
+  const [globalFilter, setGlobalFilter] = React.useState("");
+
+  const getData = async () => {
+    const user = await getUsers(5);
+    setData(user.users);
+  };
 
   const table = useReactTable({
     data,
     columns,
     state: {
       columnPinning,
+      globalFilter,
     },
     getCoreRowModel: getCoreRowModel(),
     onColumnPinningChange: setColumnPinning,
+    onGlobalFilterChange: setGlobalFilter,
+    getSortedRowModel: getSortedRowModel(),
+    debugTable: true,
   });
 
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
-    <Box sx={{ width: "100%", textAlign: "end" }}>
+    <Box style={{ width: "100%", textAlign: "end" }}>
       <Button
         sx={{ margin: "10px" }}
         onClick={colorMode.toggleColorMode}
@@ -110,64 +104,96 @@ const CustomTable = () => {
       >
         {theme.palette.mode === "dark" ? "light" : "dark"}
       </Button>
+
       <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead>
-            {table.getHeaderGroups().map((headerGroup) => (
-              <TableRow key={headerGroup.id}>
-                {headerGroup.headers.map((header) => {
-                  return (
-                    <TableCell key={header.id}>
-                      {header.isPlaceholder
-                        ? null
-                        : flexRender(
-                            header.column.columnDef.header,
-                            header.getContext()
-                          )}
-                      {!header.isPlaceholder && header.column.getCanPin() && (
-                        <div style={{ display: "inline" }}>
-                          {!header.column.getIsPinned() ? (
-                            <IconButton
-                              onClick={() => {
-                                header.column.pin("left");
-                              }}
-                            >
-                              <PushPinIcon sx={{ width: "20px" }} />
-                            </IconButton>
-                          ) : (
-                            <IconButton
-                              onClick={() => {
-                                header.column.pin(false);
-                              }}
-                            >
-                              <CancelIcon sx={{ width: "20px" }} />
-                            </IconButton>
-                          )}
-                        </div>
-                      )}
-                    </TableCell>
-                  );
-                })}
-              </TableRow>
-            ))}
-          </TableHead>
-          <TableBody>
-            {table.getRowModel().rows.map((row) => {
-              return (
-                <TableRow key={row.id}>
-                  {row.getVisibleCells().map((cell) => (
-                    <TableCell key={cell.id}>
-                      {flexRender(
-                        cell.column.columnDef.cell,
-                        cell.getContext()
-                      )}
-                    </TableCell>
-                  ))}
+        <Input
+          placeholder="Search"
+          startAdornment={
+            <InputAdornment position="end">
+              <IconButton size="small">
+                <SearchIcon />
+              </IconButton>
+            </InputAdornment>
+          }
+        />
+        <Box>
+          <Table
+            sx={{ minWidth: 650, overflow: "scroll" }}
+            aria-label="simple table"
+          >
+            <TableHead>
+              {table.getHeaderGroups().map((headerGroup) => (
+                <TableRow key={headerGroup.id}>
+                  {headerGroup.headers.map((header) => {
+                    return (
+                      <TableCell key={header.id}>
+                        {header.isPlaceholder
+                          ? null
+                          : flexRender(
+                              header.column.columnDef.header,
+                              header.getContext()
+                            )}
+                        <IconButton size="small">
+                          <MoreVertIcon />
+                        </IconButton>
+                        {!header.isPlaceholder && header.column.getCanPin() && (
+                          <div style={{ display: "inline" }}>
+                            {!header.column.getIsPinned() ? (
+                              <IconButton
+                                onClick={() => {
+                                  header.column.pin("left");
+                                }}
+                              >
+                                <PushPinIcon sx={{ width: "20px" }} />
+                              </IconButton>
+                            ) : (
+                              <IconButton
+                                onClick={() => {
+                                  header.column.pin(false);
+                                }}
+                              >
+                                <CancelIcon sx={{ width: "20px" }} />
+                              </IconButton>
+                            )}
+                          </div>
+                        )}
+                        {header.id !== "id" && (
+                          <Input
+                            placeholder={`Filter by ${header.column.columnDef.header}`}
+                            endAdornment={
+                              <InputAdornment position="end">
+                                <IconButton size="small">
+                                  <CloseIcon />
+                                </IconButton>
+                              </InputAdornment>
+                            }
+                          />
+                        )}
+                      </TableCell>
+                    );
+                  })}
                 </TableRow>
-              );
-            })}
-          </TableBody>
-        </Table>
+              ))}
+            </TableHead>
+            <TableBody>
+              {table.getRowModel().rows.map((row) => {
+                return (
+                  <TableRow key={row.id}>
+                    {row.getVisibleCells().map((cell) => (
+                      <TableCell key={cell.id}>
+                        {flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext()
+                        )}
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        </Box>
+        <div style={{ textAlign: "initial" }}>Fetched 25 of 200 total rows</div>
       </TableContainer>
     </Box>
   );
