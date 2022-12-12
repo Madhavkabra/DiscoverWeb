d2a368a39401b40e8508dd6710d3369fbbb343c8
Madhav K <madhav.kabra1100@gmail.com>
2022-12-12 20:52:33 +0530

    Refactor get paginated user's API util

--- src/services/axios/mockAdapter/users/utils/getPaginatedUsers.js
+++ src/services/axios/mockAdapter/users/utils/getPaginatedUsers.js
@@ -1,16 +1,9 @@
-import { ApiResStatus } from '../../../../../constants/apiResStatus';
 import mockUserData from '../../../../../__mocks__/users.json';
 
-const FETCH_USERS = 10;
+export const FETCH_USERS = 10;
 
-export const getPaginatedUsers = (config) => {
-  const page = config.params.page;
+export const getPaginatedUsers = (page) => {
   const paginatedUsers = mockUserData.slice(0, page * FETCH_USERS);
 
-  const data = {
-    users: paginatedUsers,
-    totalUsers: mockUserData.length,
-  };
-
-  return [ApiResStatus.SUCCESS, data];
+  return paginatedUsers;
 };
