b6c65a0a0d8480ceea8d4c70696878861eea620e
Madhav K <madhav.kabra1100@gmail.com>
2022-12-12 20:53:00 +0530

    Refactor get sorted user's API util

--- src/services/axios/mockAdapter/users/utils/getSortedUsers.js
+++ src/services/axios/mockAdapter/users/utils/getSortedUsers.js
@@ -1,14 +1,7 @@
-import { ApiResStatus } from '../../../../../constants/apiResStatus';
 import { sortArrayElementsByKey } from '../../../../../utils/sortArrayElementsByKey';
 
-export const getSortedUsers = (config) => {
-  const sortOrder = config.params.sortOrder;
-  const sortBy = config.params.sortBy;
-  const users = config.params.users;
-
+export const getSortedUsers = (users, sortBy, sortOrder) => {
   const sortedUsers = sortArrayElementsByKey(users, sortBy, sortOrder);
 
-  const data = { users: sortedUsers };
-
-  return [ApiResStatus.SUCCESS, data];
+  return sortedUsers;
 };
