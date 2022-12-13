ce8b9eb816184f88c080ee4dc89052bfdbb15a7b
Madhav K <madhav.kabra1100@gmail.com>
2022-12-13 10:22:29 +0530

    Column sorting

--- src/Components/CustomTable/CustomTable.jsx
+++ src/Components/CustomTable/CustomTable.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback } from 'react';
+import React, { useEffect } from 'react';
 import Box from '@mui/material/Box';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -8,7 +8,6 @@ import TableSortLabel from '@mui/material/TableSortLabel';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import Button from '@mui/material/Button';
 import Input from '@mui/material/Input';
 import IconButton from '@mui/material/IconButton';
 import PushPinIcon from '@mui/icons-material/PushPin';
@@ -19,22 +18,19 @@ import CloseIcon from '@mui/icons-material/Close';
 import DarkModeIcon from '@mui/icons-material/DarkMode';
 import LightModeIcon from '@mui/icons-material/LightMode';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
-import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
-import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
-import { visuallyHidden } from '@mui/utils';
 import { useTheme } from '@mui/material/styles';
 import {
   createColumnHelper,
   flexRender,
   useReactTable,
   getCoreRowModel,
-  getSortedRowModel,
 } from '@tanstack/react-table';
 import { ColorModeContext } from '../../App';
 import {
   getGlobalSearchedUsers,
   getUsers,
   getSearchedUsersByColumn,
+  getSortedUsersByColumn,
 } from '../../services/axios/users';
 
 const columnHelper = createColumnHelper();
@@ -80,7 +76,8 @@ const CustomTable = () => {
   const colorMode = React.useContext(ColorModeContext);
   const [data, setData] = React.useState([]);
   const [columnPinning, setColumnPinning] = React.useState({});
-  const [globalFilter, setGlobalFilter] = React.useState('');
+  const [order, setOrder] = React.useState('asc');
+  const [orderBy, setOrderBy] = React.useState('');
 
   const getData = async () => {
     const user = await getUsers(1);
@@ -92,25 +89,33 @@ const CustomTable = () => {
     columns,
     state: {
       columnPinning,
-      globalFilter,
     },
     getCoreRowModel: getCoreRowModel(),
     onColumnPinningChange: setColumnPinning,
-    onGlobalFilterChange: setGlobalFilter,
-    getSortedRowModel: getSortedRowModel(),
     debugTable: true,
   });
 
   const handleColumnFilterChange = async (name, value) => {
     if (value) {
       if (name === 'globalSearch') {
-        const globalSearch = await getGlobalSearchedUsers(value, 1);
+        const globalSearch = await getGlobalSearchedUsers(value, 2);
         setData(globalSearch.users);
       } else {
         const searchData = await getSearchedUsersByColumn(name, value);
         setData(searchData.users);
       }
     }
+  };
+
+  const handleSort = async (columnName) => {
+    setOrder(order === 'asc' ? 'desc' : 'asc');
+    const sortedColumn = await getSortedUsersByColumn(
+      JSON.stringify(data),
+      columnName,
+      order.toUpperCase()
+    );
+    setOrderBy(columnName);
+    setData(sortedColumn.users);
   };
 
   useEffect(() => {
@@ -121,14 +126,14 @@ const CustomTable = () => {
     <Box style={{ width: '100%', textAlign: 'end' }}>
       <TableContainer component={Paper}>
         <Input
-          placeholder="Search"
-          name="globalSearch"
+          placeholder='Search'
+          name='globalSearch'
           onChange={(e) =>
             handleColumnFilterChange(e.target.name, e.target.value)
           }
           startAdornment={
-            <InputAdornment position="end">
-              <IconButton size="small">
+            <InputAdornment position='end'>
+              <IconButton size='small'>
                 <SearchIcon />
               </IconButton>
             </InputAdornment>
@@ -137,74 +142,80 @@ const CustomTable = () => {
         <IconButton
           sx={{ margin: '20px' }}
           onClick={colorMode.toggleColorMode}
-          variant="contained"
+          variant='contained'
         >
           {theme.palette.mode === 'dark' ? <LightModeIcon /> : <DarkModeIcon />}
         </IconButton>
         <Box>
           <Table
             sx={{ minWidth: 650, overflow: 'scroll' }}
-            aria-label="simple table"
+            aria-label='simple table'
           >
             <TableHead>
               {table.getHeaderGroups().map((headerGroup) => (
                 <TableRow key={headerGroup.id}>
                   {headerGroup.headers.map((header) => {
                     return (
-                      header.id !== 'id' && (
-                        <TableCell key={header.id}>
-                          {header.isPlaceholder
-                            ? null
-                            : flexRender(
-                                header.column.columnDef.header,
-                                header.getContext()
-                              )}
-                          <IconButton size="small">
-                            <MoreVertIcon />
-                          </IconButton>
-                          {!header.isPlaceholder &&
-                            header.column.getCanPin() && (
-                              <div style={{ display: 'inline' }}>
-                                {!header.column.getIsPinned() ? (
-                                  <IconButton
-                                    onClick={() => {
-                                      header.column.pin('left');
-                                    }}
-                                  >
-                                    <PushPinIcon sx={{ width: '20px' }} />
-                                  </IconButton>
-                                ) : (
-                                  <IconButton
-                                    onClick={() => {
-                                      header.column.pin(false);
-                                    }}
-                                  >
-                                    <CancelIcon sx={{ width: '20px' }} />
-                                  </IconButton>
-                                )}
-                              </div>
+                      <TableCell
+                        key={header.id}
+                        sortDirection={orderBy === header.id ? order : false}
+                      >
+                        {header.isPlaceholder
+                          ? null
+                          : flexRender(
+                              header.column.columnDef.header,
+                              header.getContext()
                             )}
-                          {header.id !== 'id' && (
-                            <Input
-                              placeholder={`Filter by ${header.column.columnDef.header}`}
-                              name={`${header.id}`}
-                              onChange={(e) =>
-                                handleColumnFilterChange(
-                                  e.target.name,
-                                  e.target.value
-                                )
-                              }
-                              endAdornment={
-                                <InputAdornment position="end">
-                                  <IconButton size="small">
-                                    <CloseIcon />
-                                  </IconButton>
-                                </InputAdornment>
-                              }
-                            />
-                          )}
-                        </TableCell>
-                      )
+                        <IconButton size='small'>
+                          <MoreVertIcon />
+                        </IconButton>
+                        {!header.isPlaceholder && header.column.getCanPin() && (
+                          <div style={{ display: 'inline' }}>
+                            {!header.column.getIsPinned() ? (
+                              <IconButton
+                                onClick={() => {
+                                  header.column.pin('left');
+                                }}
+                              >
+                                <PushPinIcon sx={{ width: '20px' }} />
+                              </IconButton>
+                            ) : (
+                              <IconButton
+                                onClick={() => {
+                                  header.column.pin(false);
+                                }}
+                              >
+                                <CancelIcon sx={{ width: '20px' }} />
+                              </IconButton>
+                            )}
+                          </div>
+                        )}
+                        {header.id !== 'id' && (
+                          <Input
+                            placeholder={`Filter by ${header.column.columnDef.header}`}
+                            name={`${header.id}`}
+                            onChange={(e) =>
+                              handleColumnFilterChange(
+                                e.target.name,
+                                e.target.value
+                              )
+                            }
+                            endAdornment={
+                              <InputAdornment position='end'>
+                                <IconButton size='small'>
+                                  <CloseIcon />
+                                </IconButton>
+                              </InputAdornment>
+                            }
+                          />
+                        )}
+
+                        <TableSortLabel
+                          active={orderBy === header.id}
+                          direction={order}
+                          onClick={() => handleSort(header.id)}
+                        ></TableSortLabel>
+                      </TableCell>
                     );
                   })}
                 </TableRow>
